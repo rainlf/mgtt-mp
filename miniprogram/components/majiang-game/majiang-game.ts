d0d8e34e733d01d9e33ac3bfca787b8099c73212
rain <iheyu22@gmail.com>
2025-03-12 14:33:24 +0800

    add game type in game submit block

--- miniprogram/components/majiang-game/majiang-game.ts
+++ miniprogram/components/majiang-game/majiang-game.ts
@@ -6,16 +6,28 @@ Component({
         }
     },
 
+    data: {
+        winType: '平胡' // 默认选中平胡
+    },
+
     methods: {
         closeDrawer() {
             console.log('close drawer')
-            this.setData({ showDrawer: false })
+            this.setData({showDrawer: false})
 
         },
 
         submit() {
             console.log('submit')
-            this.setData({ showDrawer: false })
+            this.setData({showDrawer: false})
+        },
+
+        // 选择胡牌类型
+        selectWinType(e: any) {
+            console.log('selectWinType')
+            const type = e.currentTarget.dataset.type;
+            this.setData({winType: type});
+            console.log('selectWinType, type', type);
         }
     }
 })
