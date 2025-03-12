55bfe8711b3758d8872b63960dc50a89343ced29
rain <iheyu22@gmail.com>
2025-03-12 09:51:51 +0800

    add delete button for majiang game log

--- miniprogram/components/majiang-log/majiang-log.ts
+++ miniprogram/components/majiang-log/majiang-log.ts
@@ -33,6 +33,45 @@ Component({
             } catch (e) {
                 console.error(e)
             }
-        }
+        },
+
+        // 显示删除确认弹窗
+        showDeleteConfirm(e: any) {
+            wx.showModal({
+                title: '删除确认',
+                content: '确定要删除这条对局记录吗？',
+                confirmText: "确定",
+                cancelText: "再想想",
+                success: (res) => {
+                    if (res.confirm) {
+                        this.deleteRecord(e.currentTarget.dataset.id);
+                    }
+                }
+            });
+        },
+
+        // 执行删除操作
+        deleteRecord(gameId: number) {
+            console.log('deleteRecord', gameId);
+            if (!gameId) return;
+
+            // wx.request({
+            //     url: 'https://your-api.com/record/delete',
+            //     method: 'DELETE',
+            //     data: { id: this.data.currentDeleteId },
+            //     success: (res) => {
+            //         if (res.statusCode === 200) {
+            //             this.updateList();
+            //             wx.showToast({ title: '删除成功' });
+            //         }
+            //     },
+            //     fail: () => {
+            //         wx.showToast({ title: '删除失败', icon: 'none' });
+            //     }
+            // });
+            this.setData({
+                listData: this.data.listData.filter(item => (item.id !== gameId)),
+            })
+        },
     }
 })
